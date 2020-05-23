8fb04a5968b9558e054be0a39c3b152f3a1b2842
Pino Leonardi <p.leonardi@fablabimperia.org>
2020-05-23 19:33:52 +0200

    controlli

--- todolist/main.js
+++ todolist/main.js
@@ -1,3 +1,16 @@
+let caselladitesto = document.querySelector("#todolist");
+caselladitesto.addEventListener("keydown", function(){
+    let testo = document.querySelector("#todolist").value;
+    if (testo===""){
+        const btn = document.getElementById("inserisci");
+        btn.disabled=true;
+    }
+    else {
+        const btn = document.getElementById("inserisci");
+        btn.disabled=false;
+    }
+});
+
 const bottone = document.querySelector("#inserisci");
 bottone.addEventListener("click", function() {
   let testo = document.querySelector("#todolist").value;

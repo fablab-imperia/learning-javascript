dc38cdc9ef552869feecd410b3effc23ae7c1938
Pino Leonardi <p.leonardi@fablabimperia.org>
2020-05-31 18:37:04 +0200

    prima versione modulo

--- MMM-Todolist/MMM-Todolist.js
+++ MMM-Todolist/MMM-Todolist.js
@@ -4,7 +4,7 @@ Module.register("MMM-Todolist",{
     },
     getScripts: function() {
         return [
-            this.file('main.js') // this file will be loaded straight from the module folder.
+           // this.file('main.js') // this file will be loaded straight from the module folder.
         ]
     },
     getStyles: function() {
@@ -14,7 +14,38 @@ Module.register("MMM-Todolist",{
     }, 
     getDom: function() {
         var wrapper = document.createElement("div");
-        wrapper.innerHTML = 'Hello world!';
+        // <h1 class="colorerosso">La mia todolist preferita</h1>
+        let h1 = document.createElement("h1");
+        h1.innerText = "La mia todolist preferita";
+        h1.className = "colorerosso";
+        wrapper.appendChild(h1);
+        /*  <form>
+        <label class="colorerosso"  for="todolist">Cose da fare:</label>
+        <input type="text" id="todolist" ><br><br>
+        <input disabled type="button" value="Inserisci" id="inserisci">
+        </form> */
+        let form = document.createElement("form");
+        let label = document.createElement("label");
+        label.className = "colorerosso";
+        label.innerText = "Cose da fare";
+        form.appendChild(label);
+        let input = document.createElement("input");
+        input.type = "text";
+        input.id = "todolist";
+        form.appendChild(input);
+        let br = document.createElement("br");
+        form.appendChild(br);
+        let button = document.createElement("input");
+        button.type = "button";
+        button.id = "inserisci";
+        button.value = "Inserisci";
+        form.appendChild(button);
+        wrapper.appendChild(form);
+        /* <ul id="lista">
+    </ul> */
+        let ul = document.createElement("ul");
+        ul.id = "lista";
+        wrapper.appendChild(ul);
         return wrapper;
     }
 
